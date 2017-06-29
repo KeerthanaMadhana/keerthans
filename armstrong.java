801b695492b979a8d6bc83871d9ec7d1da23206b
KeerthanaMadhana <keerthana19963071@gmail.com>
2017-06-29 12:20:53 +0530

    Update armstrong.java

--- armstrong.java
+++ armstrong.java
@@ -4,26 +4,27 @@ import java.util.Scanner;
 
 public class armstrong {
 	public static void main(String[] args) {
-		int a=0,b,temp;
-		int n;
 		Scanner s=new Scanner(System.in);
-		System.out.println("Enter the number");
-		n=s.nextInt();
-		temp=n;
-		while (n>0)
-		{
-			b=n%10;
-			n=n/10;
-			a=a+(b*b*b);
-		}
-		if (temp==a)
-		{
-			System.out.println("armstrong number");
-		}
-		else
-		{
-			System.out.println("not a armstrong number");
-		}
+	        int range=s.nextInt();
+		int a,b,csum,count=0;
+		b=1
+			while(b<=range)
+			{
+				sum=0;
+				c=b;
+				while(c>0)
+				{
+					a=c%10;
+					sum=sum+(a*a*a);
+					c=c/10;
+				}
+				if(sum==b)
+				{
+					System.out.println(b+"armstrong no");
+					count=count+1;
+				}
+				b++;
+			}
+		System.out.println("armstrong no present in range"+count);
 	}
-
 }
