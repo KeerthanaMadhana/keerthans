99a7e34ebb4974b2ec4163621ac3a7adb597f2eb
KeerthanaMadhana <keerthana19963071@gmail.com>
2017-06-29 11:46:10 +0530

    Create intervaleven.java

---
+++ intervaleven.java
@@ -0,0 +1,19 @@
+package keertha;
+
+public class intervaleven {
+	public static void main(String[] args) {
+		int i;
+		for(i=1;i<=30;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println(i+" ");
+			}
+		}
+
+	}
+
+}
+
+	
+
